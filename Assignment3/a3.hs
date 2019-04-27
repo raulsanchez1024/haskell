7040a5ef5a3b9b34dc50d0f9970d5cfd4a99993b
Raul Sanchez <raulsanchez1024@gmail.com>
2019-04-27 15:25:32 -0600

    Made some changes to a3.hs

--- Assignment3/a3.hs
+++ Assignment3/a3.hs
@@ -1,23 +1,33 @@
+-- Raul Sanchez
+-- Assignment3
+
 import Data.List
 import Data.Char
 
+-- 1.
 -- onlyLowercase
--- Consume
--- Produce
+-- Consume a list of strings
+-- Produce/return a list of strings that start with a lower case letter
 onlyLowercase :: [String] -> [String]
 onlyLowercase = filter (\x -> ("" /= x) && (isLower $ head x));
 
--- foldl (\x xs -> if length x < length xs then xs else x)
--- foldl (\accmax xs -> if length accmax < length xs then xs else accmax) [] ["one", "two", "three", "four", "five"]
-
+-- 2.
 -- longestString
--- Consume
--- Produce
+-- Consume a list of strings
+-- Produce/return the longest string. NOTE: if two words have the save
+-- length, then return the first longest string
 longestString :: [String] -> String
-longestString xs = foldl (\x acc -> if length x > length acc then x else acc) "" xs
+longestString xs = foldl (\x acc -> if length acc > length x then acc else x) "" xs
 
+-- 3.
 -- longestString'
--- Consume
--- Produce
+-- Consume a list of strings
+-- Produce/return the longest string. NOTE: if two words have the save
+-- length, then return the last longest string
 longestString' :: [String] -> String
 longestString' xs = foldl (\x acc -> if length x > length acc then x else acc) "" xs
+
+-- 4.1
+-- longestString3
+-- Consume
+-- Produce
