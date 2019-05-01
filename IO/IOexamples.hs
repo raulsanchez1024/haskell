7ce186c9191a4f3089059c377464d4cd2959db7d
Raul Sanchez <raulsanchez1024@gmail.com>
2019-05-01 15:28:50 -0600

    First look at IO

---
+++ IO/IOexamples.hs
@@ -0,0 +1,12 @@
+-- main = putStrLn "Hello, World!"
+
+-- main = do
+--   putStr "Enter your name: "
+--   name <- getLine
+--   putStrLn ("Hello " ++ name)
+
+main = do
+  let
+    x = 5
+    y = 6
+  putStrLn (show(x + y))
