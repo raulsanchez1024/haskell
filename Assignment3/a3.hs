496cc560bdbfce9d81a1ca27f1b7c84de3868c9f
Raul Sanchez <raulsanchez1024@gmail.com>
2019-05-03 18:55:12 -0600

    WTF pattern mathcing

--- Assignment3/a3.hs
+++ Assignment3/a3.hs
@@ -1,8 +1,11 @@
 -- Raul Sanchez
 -- Assignment3
 
+module A3b where
 import Data.List
 import Data.Char
+
+-- ========== PART I ========== --
 
 -- 1.
 -- onlyLowercase
@@ -17,7 +20,8 @@ onlyLowercase = filter (\x -> ("" /= x) && (isLower $ head x));
 -- Produce/return the longest string. NOTE: if two words have the save
 -- length, then return the first longest string
 longestString :: [String] -> String
-longestString xs = foldl (\x acc -> if length acc > length x then acc else x) "" xs
+longestString xs =
+  foldl (\x acc -> if length x > length acc then x else acc) "" xs
 
 -- 3.
 -- longestString'
@@ -25,25 +29,81 @@ longestString xs = foldl (\x acc -> if length acc > length x then acc else x) ""
 -- Produce/return the longest string. NOTE: if two words have the save
 -- length, then return the last longest string
 longestString' :: [String] -> String
-longestString' xs = foldl (\x acc -> if length x > length acc then x else acc) "" xs
+longestString' xs =
+  foldl (\x acc -> if length acc <= length x then x else acc) "" xs
 
--- 4.1.a
--- longestString3
--- Consume
--- Produce
-longestString3 :: [String] -> String
-longestString3 xs = foldl (\x acc -> if length acc > length x then acc else x) "" xs
+-- 4.1
+-- longestStringHelper
+-- Consume a function that evaluates a Bool
+-- Produce/return longest String in the list
+longestStringHelper :: (Int -> Int -> Bool) -> [String] -> String
+longestStringHelper _ [] = []
+longestStringHelper a (x:xs) =
+  foldl (\b acc -> if a (length acc) (length b) then acc else b) x xs
 
--- 4.1.b
--- longestString4
--- Consume
--- Produce
-longestString4 :: [String] -> String
-longestString4 xs = foldl (\x acc -> if length x > length acc then x else acc) "" xs
 
 -- 4.2
--- longestStringHelper
--- Consume
--- Produce
-longestStringHelper :: (Int -> Int -> Bool) -> [String] -> String
-longestStringHelper f =
+-- longestString3
+-- Consume a list of Strings
+-- Produce/return the longest string
+longestString3 :: [String] -> String
+longestString3 = longestStringHelper (>)
+
+-- 4.3
+-- longestString4
+-- Consume a list of Strings
+-- Produce/return the longest string
+longestString4 :: [String] -> String
+longestString4 = longestStringHelper (<=)
+
+-- 5.
+-- longestLowercase
+-- Consume a list of Strings
+-- Produce/return longest lowercase string
+longestLowercase :: [String] -> String
+longestLowercase = longestLowercase . onlyLowercase
+
+-- 6.
+-- revStringRev
+-- Consume a String
+-- Produce a reversed and converted to lowercase string
+revStringRev :: String -> String
+revStringRev = reverse . map toLower
+
+-- 7.
+-- firstAnswer
+-- Consume <a> variable and Maybe <b>
+-- Produce/return Maybe <b>
+firstAnswer :: (Eq b) => ( a -> Maybe b ) -> [a] -> Maybe b
+firstAnswer _ [] = Nothing
+firstAnswer a (x:xs) | a x /= Nothing = a x | otherwise = firstAnswer a xs
+
+-- 8.
+-- allAnswers
+-- Consume <a> variable and Maybe a list of <b>
+-- Produce/return Maybe a list of <b>
+allAnswers :: (a -> Maybe [b]) -> [a] -> Maybe [b]
+allAnswers _ [] = Just []
+
+-- ========== PART II ========== --
+
+data Pattern = WildcardPat | VariablePat (String) | UnitPat | ConstantPat (Int) | ConstructorPat (String, Pattern) | TuplePat ([Pattern]) deriving (Eq, Show)
+data Value = Constant (Int) | Unit | Constructor (String, Value) | Tuple [Value] deriving (Eq, Show)
+
+-- 1.
+-- checkPat
+-- Consume a Pattern
+-- Produce a boolean
+-- checkPat :: Pattern -> Bool
+
+-- 2.
+-- match
+-- Consume a Tuple of Value and Patter
+-- Produce/return maybe list of Tuples of String and Value
+-- match  (Value, Pattern) -> Maybe [(String, Value)]
+
+-- 3.
+-- firstMatch
+-- Consume a Value
+-- Produce/return Maybe a list of tuples of tupe String and Value
+-- firstMatch :: Value -> [Pattern] -> Maybe [(String, Value)]
